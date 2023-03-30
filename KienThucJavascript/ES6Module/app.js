75e785a0f0d6e134ea739b3afd1f66a99c711e4c
duynghiadev <duynghia22302@gmail.com>
2023-03-30 07:48:44 +0700

    Edit code in ES6 module

--- KienThucJavascript/ES6Module/app.js
+++ KienThucJavascript/ES6Module/app.js
@@ -1,7 +1,10 @@
-import React, { handle, value } from "./react.js";
+import React, { handle, value as value1 } from "./react.js";
+
+const value = 200;
 
 console.log(React);
 
 handle();
 
 console.log(value);
+console.log(value1);
