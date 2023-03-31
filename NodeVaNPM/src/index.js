4ee6fcdaf800ddee23dc29317f2a0c909028f73c
duynghiadev <duynghia22302@gmail.com>
2023-03-31 07:26:52 +0700

    Xử lý Loader và CSS trong Webpack

--- NodeVaNPM/src/index.js
+++ NodeVaNPM/src/index.js
@@ -1,3 +1,5 @@
 import sum from "./utils";
+import "./styles/style.css";
+import "./styles/style.scss";
 
 console.log(sum(1, 3));
--- NodeVaNPM/webpack.config.js
+++ NodeVaNPM/webpack.config.js
@@ -5,11 +5,19 @@ console.log("path.resolve()", path.resolve());
 console.log(`path.resolve(__dirname, 'dist')`, path.resolve(__dirname, "dist"));
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: {
     app: path.resolve("src/index.js"),
   },
   output: {
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss|css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
 };
