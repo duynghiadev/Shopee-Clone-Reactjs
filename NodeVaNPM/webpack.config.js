4f48e83905c13156ac6cf00342962ad2ccce83ff
duynghiadev <duynghia22302@gmail.com>
2023-03-31 09:15:51 +0700

    Sourcemap cho webpack

--- NodeVaNPM/webpack.config.js
+++ NodeVaNPM/webpack.config.js
@@ -6,42 +6,47 @@ console.log("__dirname", __dirname);
 console.log("path.resolve()", path.resolve());
 console.log(`path.resolve(__dirname, 'dist')`, path.resolve(__dirname, "dist"));
 
-module.exports = {
-  mode: "production",
-  entry: {
-    app: path.resolve("src/index.js"),
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss|css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-    }),
-    new HtmlWebpackPlugin({
-      title: "Webpack App",
-      filename: "index.html",
-      template: "src/template.html",
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: "dist", // Đường dẫn tương đối đến với thư mục chứa index.html
+module.exports = (env) => {
+  console.log(process.env.NODE_ENV);
+  const isDevelopment = process.env.NODE_ENV === "development";
+  return {
+    mode: isDevelopment ? "development" : "production",
+    entry: {
+      app: path.resolve("src/index.js"),
     },
-    port: 3000, // Port thay cho port mặc định (8080)
-    open: true, // Mở trang webpack khi chạy terminal
-    hot: true, // Bật tính năng reload nhanh Hot Module Replacement
-    compress: true, // Bật Gzip cho các tài nguyên
-    historyApiFallback: true, // Set true nếu bạn dùng cho các SPA và sử dụng History API của HTML5
-  },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].[contenthash].js",
+      clean: true,
+    },
+    devtool: isDevelopment ? "source-map" : false,
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss|css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].[contenthash].css",
+      }),
+      new HtmlWebpackPlugin({
+        title: "Webpack App",
+        filename: "index.html",
+        template: "src/template.html",
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: "dist", // Đường dẫn tương đối đến với thư mục chứa index.html
+      },
+      port: 3000, // Port thay cho port mặc định (8080)
+      open: true, // Mở trang webpack khi chạy terminal
+      hot: true, // Bật tính năng reload nhanh Hot Module Replacement
+      compress: true, // Bật Gzip cho các tài nguyên
+      historyApiFallback: true, // Set true nếu bạn dùng cho các SPA và sử dụng History API của HTML5
+    },
+  };
 };
