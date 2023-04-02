e241bf2afd65254923a5b19dc1d69ad98e90df99
duynghiadev <duynghia22302@gmail.com>
2023-04-03 05:22:29 +0700

    componentdidMount

--- ReactMainConcept/my-app/src/Clock.jsx
+++ ReactMainConcept/my-app/src/Clock.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
+const lists = ["BMW", "Toyota", "Honda"];
+
+const fetchApi = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(lists);
+    }, 1000);
+  });
+};
+
 export default class Clock extends React.Component {
   constructor(props) {
     super(props);
@@ -11,9 +21,22 @@ export default class Clock extends React.Component {
         created: new Date().getSeconds(),
         end: 121212,
       },
+      lists: [],
     };
     this.date = "3/4/2023";
     this.getTime = this.getTime.bind(this);
+  }
+
+  componentDidMount() {
+    const seconds = document.getElementById("seconds");
+    console.log(seconds);
+
+    fetchApi().then((res) =>
+      this.setState((prevState) => ({
+        ...prevState,
+        lists: res,
+      }))
+    );
   }
 
   getTime() {
@@ -28,11 +51,12 @@ export default class Clock extends React.Component {
   }
 
   render() {
+    console.log(this.state);
     return (
       <div>
         <h1>Hello, world!, {this.props.name}</h1>
         <h2>It is {this.state.time.created}</h2>
-        <h2>It is {this.state.seconds.created}</h2>
+        <h2 id="seconds">It is {this.state.seconds.created}</h2>
         <h2>It is {this.date}</h2>
         <button onClick={this.getTime}>Get Time</button>
       </div>
