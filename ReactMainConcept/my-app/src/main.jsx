90741a2a5541a8e89564d472a157417e224fd264
duynghiadev <duynghia22302@gmail.com>
2023-04-03 03:41:55 +0700

    Render element

--- ReactMainConcept/my-app/src/main.jsx
+++ ReactMainConcept/my-app/src/main.jsx
@@ -3,8 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello World!</h1>
+      <h2>it is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  root.render(element);
+}
+
+setInterval(tick, 1000);
