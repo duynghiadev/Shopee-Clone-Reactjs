6b603eb661cfc44e32c599812b682b984e3daefc
duynghiadev <duynghia22302@gmail.com>
2023-04-06 06:28:45 +0700

    Vấn đề Global Scoped CSS trong Reac

---
+++ CSSTrongReact/my-app/src/Cart.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./cart.css";
+
+export default function Cart() {
+  return <div className="container">Cart</div>;
+}
---
+++ CSSTrongReact/my-app/src/ProductList.jsx
@@ -0,0 +1,6 @@
+import "./productList.css";
+import React from "react";
+
+export default function ProductList() {
+  return <div className="container">ProductList</div>;
+}
