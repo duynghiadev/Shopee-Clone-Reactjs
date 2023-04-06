cb8c417013027e8f815daefdb02c2545c9bdd323
duynghiadev <duynghia22302@gmail.com>
2023-04-06 07:18:22 +0700

    Sử dụng CSS module trong React

--- CSSTrongReact/my-app/src/ProductList.jsx
+++ CSSTrongReact/my-app/src/ProductList.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import "./productList.scss";
+import styles from "./productList.module.scss";
 
 export default function ProductList() {
-  return <div className="container">ProductList</div>;
+  return (
+    <div className="container">
+      ProductList
+      <div className={styles.productList}>
+        <div className={styles.productItem}>Item</div>
+        <div className={styles.productItem}>Item</div>
+        <div className={styles.productItem}>Item</div>
+        <div className={styles.productItem}>Item</div>
+      </div>
+    </div>
+  );
 }
