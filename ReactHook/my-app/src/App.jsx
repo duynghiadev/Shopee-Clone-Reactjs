c79a7aca2ebb5e37dcf3f674c5be7ec72fb45a2e
duynghiadev <duynghia22302@gmail.com>
2023-04-17 05:30:41 +0700

    102. useEffect

--- ReactHook/my-app/src/App.jsx
+++ ReactHook/my-app/src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 import User from "./User";
 import UserClassComponent from "./User.class";
 
 function App() {
+  const [isShow, setIsShow] = useState(true);
+
   return (
     <div className="App">
-      <UserClassComponent />
-      <User />
+      {/* <UserClassComponent /> */}
+      {isShow && <User />}
+
+      <button onClick={() => setIsShow((prevState) => !prevState)}>
+        Change isShow
+      </button>
     </div>
   );
 }
--- ReactHook/my-app/src/User.jsx
+++ ReactHook/my-app/src/User.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const initialAddress = () => {
   console.log("Initial address");
@@ -11,6 +11,20 @@ const initialAddress = () => {
   };
 };
 
+const getAddress = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        nation: "USA",
+        city: {
+          street: "100 Nicolas, New York",
+          house: "Building",
+        },
+      });
+    }, 3000);
+  });
+};
+
 export default function User() {
   const [firstName, setFirstName] = useState("Alex");
   const [age, setAge] = useState(24);
@@ -18,6 +32,8 @@ export default function User() {
   const [address, setAddress] = useState(initialAddress);
 
   const [, forceRerender] = useState(0);
+
+  const profile = {};
 
   const increaseAge = () => {
     setAge((prevAge) => prevAge + 1);
@@ -45,6 +61,45 @@ export default function User() {
   };
 
   console.log("Re-render");
+
+  /**
+   * Giống componentDidUpdate, effect function chạy lại mỗi khi component re-render
+   */
+
+  // useEffect(() => {
+  //   console.log(document.getElementsByTagName("li"));
+  //   console.log("useEffect giống componentDidUpdate");
+  // });
+
+  /**
+   * Thường dùng để gọi API
+   */
+
+  useEffect(() => {
+    console.log(document.getElementsByTagName("li"));
+    console.log("useEffect giống componentDidMount");
+
+    console.log(profile);
+    getAddress().then((res) => {
+      setAddress((prevAddress) => {
+        const newAddress = { ...prevAddress };
+        newAddress.city = res.city;
+        return newAddress;
+      });
+    });
+
+    // Cleanup function
+    return () => {
+      console.log("Huỷ gọi API");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("age: ", age);
+    return () => {
+      console.log("Clean Age");
+    };
+  }, [age]);
 
   return (
     <div>
