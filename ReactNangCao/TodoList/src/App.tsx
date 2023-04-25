cf37e447836378ff9bda756b00249b1fc478b670
duynghiadev <duynghia22302@gmail.com>
2023-04-26 03:57:03 +0700

    131. Kỹ thuật Render Props

--- ReactNangCao/TodoList/src/App.tsx
+++ ReactNangCao/TodoList/src/App.tsx
@@ -1,15 +1,37 @@
+import { useCallback, useMemo, useRef, useState } from 'react'
+import Ads from './components/Ads'
 import AutoInput from './components/AutoInput'
 import Count from './components/Count'
 import Counter from './components/Counter'
 import Manager from './components/Manager'
+import MouseTracker from './components/MouseTracker'
 import ProductList from './components/ProductList'
 import Slider from './components/Slider'
 import TodoList from './components/TodoList'
 import Watch from './components/Watch'
 import Welcome from './components/Welcome'
 import MainLayout from './layouts/MainLayout'
+import { PositionType } from './components/MouseTracker/MouseTracker'
+
+// Chúng ta có thể để biến ở ngoài
+// const renderAds = (value: PositionType) => <Ads {...value} visible
 
 function App() {
+  const [, render] = useState({})
+
+  // Đây là cách sử dụng useRef
+
+  const renderRef = useRef<any>((value: PositionType) => <Ads {...value} visible />)
+  // Có 3 cách sử dụng React memo: useCallback, useMemo, useRef
+
+  // Đây là cách sử dụng hook useCallback
+  // const renderAds = useCallback((value: PositionType) => <Ads {...value} visible />, [])
+
+  // Đây là cách sử dụng hook useMemo
+  const renderAds = useMemo(() => {
+    return (value: PositionType) => <Ads {...value} visible />
+  }, [])
+
   return (
     <div>
       {/* <TodoList /> */}
@@ -20,9 +42,14 @@ function App() {
       {/* <Welcome /> */}
       {/* <AutoInput /> */}
       {/* <ProductList /> */}
-      <MainLayout>
+      {/* <MainLayout>
         <Manager />
-      </MainLayout>
+      </MainLayout> */}
+      <div>
+        <button onClick={() => render({})}>Force Rerender</button>
+      </div>
+      <MouseTracker render={renderRef.current} />
+      {/* <Ads visible /> */}
     </div>
   )
 }
