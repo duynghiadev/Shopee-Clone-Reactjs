5ab080c424185f731912980e3b382b493acd8989
duynghiadev <duynghia22302@gmail.com>
2023-05-02 03:45:15 +0700

    134. Redux là gì? Cách mình setup dự án React Typescript. Bổ sung thêm ví dụ Redux và giải thích chit tiết code

---
+++ Redux/old-local-blog/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='App'>
+      <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+        Learn React
+      </a>
+    </div>
+  )
+}
+
+export default App
