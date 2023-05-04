4bc3462705d53d8c43ed75a2cd1f27fde3887513
duynghiadev <duynghia22302@gmail.com>
2023-05-04 07:06:58 +0700

    137. Tất cả mọi thứ bạn cần biết về createAsyncThunk

---
+++ Redux/remote-blog/src/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import blogReducer from 'pages/blog/blog.slice'
+import { useDispatch } from 'react-redux'
+
+export const store = configureStore({
+  reducer: { blog: blogReducer }
+})
+
+// Lấy RootState và AppDispatch từ store của chúng ta
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch = () => useDispatch<AppDispatch>()
