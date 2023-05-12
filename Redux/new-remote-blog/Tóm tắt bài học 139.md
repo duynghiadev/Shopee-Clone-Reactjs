71d982449028a8ff5fb579809ee650705fea6071
duynghiadev <duynghia22302@gmail.com>
2023-05-13 04:41:00 +0700

    139. Dùng mutation để POST, PUT, DELETE trong RTK Query

---
+++ Redux/new-remote-blog/Tóm tắt bài học 139.md
@@ -0,0 +1,8 @@
+## Sau khi học xong bài học `139. Dùng mutation để POST, PUT, DELETE trong RTK Query` ta tóm tắt lại bài học như sau:
+
+- Ở video trước thì chúng ta đã biết được cách dùng query để get api trong RTK Query rồi, video này chúng ta sẽ học cách dùng mutation để thực hiện những chức năng còn thiếu như Add Post, Update Post, Delete Post nhé
+
+- Trong bài này chúng ta sẽ học:
+
+  - Cách dùng mutation trong RTK Query
+  - Cơ chế auto-refetch của RTK Query dựa vào tags
--- Redux/new-remote-blog/src/pages/blog/blog.slice.ts
+++ Redux/new-remote-blog/src/pages/blog/blog.slice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 
 interface BlogState {
   postId: string
@@ -11,9 +11,19 @@ const initialState: BlogState = {
 const blogSlice = createSlice({
   name: 'blog',
   initialState,
-  reducers: {}
+  reducers: {
+    startEditPost: (state, action: PayloadAction<string>) => {
+      state.postId = action.payload
+    },
+
+    cancelEditPost: (state) => {
+      state.postId = ''
+    }
+  }
 })
 
 const blogReducer = blogSlice.reducer
+
+export const { cancelEditPost, startEditPost } = blogSlice.actions
 
 export default blogReducer
