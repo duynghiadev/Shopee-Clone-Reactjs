cd55aaa20993ac162c5349a7b571d076ad493f06
duynghiadev <duynghia22302@gmail.com>
2023-07-05 16:43:46 +0700

    146. Dùng mutation để add student, trick hay hay xử lý lỗi trong React Query

--- ReactQuery/student-management/src/pages/Students/Students.tsx
+++ ReactQuery/student-management/src/pages/Students/Students.tsx
@@ -247,7 +247,7 @@ export default function Students() {
       <div className='mt-6'>
         <Link
           to='/students/add'
-          className=' rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 '
+          className='rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 '
         >
           Add Student
         </Link>
