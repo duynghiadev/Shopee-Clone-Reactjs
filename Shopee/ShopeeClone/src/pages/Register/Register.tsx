ddb1a4de0ff308788c908a04854282498264b326
duynghiadev <duynghia22302@gmail.com>
2023-07-07 17:15:05 +0700

    160. Custom class .container bằng cách tạo plugin Tailwindcss

--- Shopee/ShopeeClone/src/pages/Register/Register.tsx
+++ Shopee/ShopeeClone/src/pages/Register/Register.tsx
@@ -31,7 +31,7 @@ export default function Register() {
 
   return (
     <div className='bg-orange'>
-      <div className='mx-auto max-w-7xl px-4'>
+      <div className='container'>
         <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
             <form className='rounded bg-white p-10 shadow-sm' onSubmit={onSubmit} noValidate>
