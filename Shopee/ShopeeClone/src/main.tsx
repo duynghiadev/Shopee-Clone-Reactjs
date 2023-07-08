15027ac3cd18ab29af62742923b4c565353e99a0
duynghiadev <duynghia22302@gmail.com>
2023-07-08 15:30:09 +0700

    163. Cấu hình Axios và React Query

--- Shopee/ShopeeClone/src/main.tsx
+++ Shopee/ShopeeClone/src/main.tsx
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from 'src/App'
 import { BrowserRouter } from 'react-router-dom'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
---
+++ Shopee/ShopeeClone/src/utils/http.ts
@@ -0,0 +1,18 @@
+import axios, { type AxiosInstance } from 'axios'
+
+class Http {
+  instance: AxiosInstance
+  constructor() {
+    this.instance = axios.create({
+      baseURL: 'https://api-ecom.duthanhduoc.com/',
+      timeout: 10000,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+}
+
+const http = new Http().instance
+
+export default http
