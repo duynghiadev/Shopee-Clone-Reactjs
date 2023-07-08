0db33aecce4052b0f8475f94fff526fe64dfaa23
duynghiadev <duynghia22302@gmail.com>
2023-07-08 17:12:36 +0700

    165. Xử lý lỗi 422 từ server trả về

--- Shopee/ShopeeClone/src/pages/Register/Register.tsx
+++ Shopee/ShopeeClone/src/pages/Register/Register.tsx
@@ -6,6 +6,8 @@ import Input from 'src/components/Input'
 import { schema, Schema } from 'src/utils/rules'
 import { registerAccount } from 'src/apis/auth.api'
 import { omit } from 'lodash'
+import { isAxiosUnprocessableEntityError } from 'src/utils/utils'
+import { ResponseApi } from 'src/types/utils.type'
 
 type FormData = Schema
 
@@ -14,6 +16,7 @@ export default function Register() {
     register,
     handleSubmit,
     watch,
+    setError,
     formState: { errors }
   } = useForm<FormData>({
     resolver: yupResolver(schema)
@@ -28,12 +31,39 @@ export default function Register() {
     registerAccountMutation.mutate(body, {
       onSuccess: (data) => {
         console.log(data)
+      },
+      onError: (error) => {
+        if (isAxiosUnprocessableEntityError<ResponseApi<Omit<FormData, 'confirm_password'>>>(error)) {
+          const formError = error.response?.data.data
+          if (formError) {
+            Object.keys(formError).forEach((key) => {
+              setError(key as keyof Omit<FormData, 'confirm_password'>, {
+                message: formError[key as keyof Omit<FormData, 'confirm_password'>],
+                type: 'Server'
+              })
+            })
+          }
+
+          // Cách 2:
+          // if (formError?.email) {
+          //   setError('email', {
+          //     message: formError.email,
+          //     type: 'Server'
+          //   })
+          // }
+          // if (formError?.password) {
+          //   setError('password', {
+          //     message: formError.password,
+          //     type: 'Server'
+          //   })
+          // }
+        }
       }
     })
   })
 
-  const value = watch()
-  console.log(value)
+  // const value = watch()
+  // console.log(value)
 
   return (
     <div className='bg-orange'>
---
+++ Shopee/ShopeeClone/src/utils/utils.ts
@@ -0,0 +1,11 @@
+import axios, { AxiosError } from 'axios'
+import HttpStatusCode from 'src/constants/httpStatusCode.enum'
+
+export function isAxiosError<T>(error: unknown): error is AxiosError<T> {
+  // eslint-disable-next-line import/no-named-as-default-member
+  return axios.isAxiosError(error)
+}
+
+export function isAxiosUnprocessableEntityError<FormError>(error: unknown): error is AxiosError<FormError> {
+  return isAxiosError(error) && error.response?.status === HttpStatusCode.UnprocessableEntity
+}
