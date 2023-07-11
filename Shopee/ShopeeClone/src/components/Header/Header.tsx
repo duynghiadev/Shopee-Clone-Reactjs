bb22fd6c76afef0f641fa7d3c2eec4a607c60034
duynghiadev <duynghia22302@gmail.com>
2023-07-11 08:53:28 +0700

    172. Setup Protected Route và Rejected Route trong React Router

--- Shopee/ShopeeClone/src/components/Header/Header.tsx
+++ Shopee/ShopeeClone/src/components/Header/Header.tsx
@@ -48,7 +48,7 @@ export default function Header() {
             renderPopover={
               <div className='relative rounded-sm border border-gray-200 bg-white shadow-md'>
                 <Link
-                  to='/'
+                  to='/profile'
                   className='block w-full bg-white px-4 py-3 text-left hover:bg-slate-100 hover:text-cyan-500'
                 >
                   Tài khoản của tôi
--- Shopee/ShopeeClone/src/useRouteElements.tsx
+++ Shopee/ShopeeClone/src/useRouteElements.tsx
@@ -1,37 +1,68 @@
-import { useRoutes } from 'react-router-dom'
-import ProductList from './pages/ProductList'
-import Login from './pages/Login'
-import RegisterLayout from './layouts/RegisterLayout'
-import Register from './pages/Register'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
+import RegisterLayout from './layouts/RegisterLayout'
+import Login from './pages/Login'
+import ProductList from './pages/ProductList'
+import Profile from './pages/Profile'
+import Register from './pages/Register'
+
+const isAuthenticated = false
+
+function ProtectedRoute() {
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+}
+
+function RejectedRoute() {
+  return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
+}
 
 export default function useRouteElements() {
   const routeElements = useRoutes([
     {
-      path: '/',
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: 'login',
+          element: (
+            <RegisterLayout>
+              <Login />
+            </RegisterLayout>
+          )
+        },
+        {
+          path: 'register',
+          element: (
+            <RegisterLayout>
+              <Register />
+            </RegisterLayout>
+          )
+        }
+      ]
+    },
+    {
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: 'profile',
+          element: (
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          )
+        }
+      ]
+    },
+    {
+      path: '',
+      index: true,
       element: (
         <MainLayout>
           <ProductList />
         </MainLayout>
       )
-    },
-    {
-      path: '/login',
-      element: (
-        <RegisterLayout>
-          <Login />
-        </RegisterLayout>
-      )
-    },
-    {
-      path: '/register',
-      element: (
-        <RegisterLayout>
-          <Register />
-        </RegisterLayout>
-      )
     }
   ])
-
   return routeElements
 }
