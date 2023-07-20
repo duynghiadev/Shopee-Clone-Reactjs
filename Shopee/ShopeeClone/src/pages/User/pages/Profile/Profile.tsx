b589d4c39f3a7405e1ec6fe7cb8b95ba88c3f1db
duynghiadev <duynghia22302@gmail.com>
2023-07-20 09:44:44 +0700

    210. Code UI trang Profile

--- Shopee/ShopeeClone/src/pages/User/pages/Profile/Profile.tsx
+++ Shopee/ShopeeClone/src/pages/User/pages/Profile/Profile.tsx
@@ -1,3 +1,75 @@
+import Input from 'src/components/Input'
+
 export default function Profile() {
-  return <div>Profile</div>
+  return (
+    <div className='rounded-sm bg-white px-2 pb-10 shadow md:px-7 md:pb-20'>
+      <div className='border-b border-b-gray-200 py-6'>
+        <h1 className='text-lg font-medium capitalize text-gray-900'>Hồ Sơ Của Tôi</h1>
+        <div className='mt-1 text-sm text-gray-700'>Quản lý thông tin hồ sơ để bảo mật tài khoản</div>
+      </div>
+      <div className='mt-8 flex flex-col-reverse md:flex-row md:items-start'>
+        <form className='mt-6 flex-grow md:mt-0 md:pr-12'>
+          <div className='flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Email</div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <div className='pt-3 text-gray-700'>du***********@gmail.com</div>
+            </div>
+          </div>
+          <div className='mt-6 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Tên</div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Số điện thoại</div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Địa chỉ</div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Ngày sinh</div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <div className='flex justify-between'>
+                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  <option disabled>Ngày</option>
+                </select>
+                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  <option disabled>Tháng</option>
+                </select>
+                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  <option disabled>Năm</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </form>
+        <div className='flex justify-center md:w-72 md:border-l md:border-l-gray-200'>
+          <div className='flex flex-col items-center'>
+            <div className='my-5 h-24 w-24'>
+              <img
+                src='https://cf.shopee.vn/file/d04ea22afab6e6d250a370d7ccc2e675_tn'
+                alt=''
+                className='w-full rounded-full object-cover'
+              />
+            </div>
+            <input className='hidden' type='file' accept='.jpg,.jpeg,.png' />
+            <button className='flex h-10 items-center justify-end rounded-sm border bg-white px-6 text-sm text-gray-600 shadow-sm'>
+              Chọn ảnh
+            </button>
+            <div className='mt-3 text-gray-400'>
+              <div>Dụng lượng file tối đa 1 MB</div>
+              <div>Định dạng:.JPEG, .PNG</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- Shopee/ShopeeClone/src/useRouteElements.tsx
+++ Shopee/ShopeeClone/src/useRouteElements.tsx
@@ -6,13 +6,13 @@ import MainLayout from './layouts/MainLayout'
 import RegisterLayout from './layouts/RegisterLayout'
 import Login from './pages/Login'
 import ProductList from './pages/ProductList'
-import Profile from './pages/Profile'
 import Register from './pages/Register'
 import ProductDetail from './pages/ProductDetail'
 import Cart from './pages/Cart'
 import CartLayout from './layouts/CartLayout'
 import UserLayout from './pages/User/layouts/UserLayout'
 import ChangePassword from './pages/User/pages/ChangePassword'
+import Profile from './pages/User/pages/Profile'
 
 function ProtectedRoute() {
   const { isAuthenticated } = useContext(AppContext)
