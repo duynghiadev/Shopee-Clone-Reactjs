9bb12c293c4f721b7616550b565bce91521e1cc7
duynghiadev <duynghia22302@gmail.com>
2023-07-26 17:15:24 +0700

    221. Thêm 404 Page

--- Shopee/ShopeeClone/src/useRouteElements.tsx
+++ Shopee/ShopeeClone/src/useRouteElements.tsx
@@ -14,6 +14,7 @@ import CartLayout from './layouts/CartLayout'
 import UserLayout from './pages/User/layouts/UserLayout'
 import ChangePassword from './pages/User/pages/ChangePassword'
 import HistoryPurchase from './pages/User/pages/HistoryPurchase'
+import NotFound from './pages/NotFound'
 
 function ProtectedRoute() {
   const { isAuthenticated } = useContext(AppContext)
@@ -100,6 +101,14 @@ export default function useRouteElements() {
       element: (
         <MainLayout>
           <ProductList />
+        </MainLayout>
+      )
+    },
+    {
+      path: '*',
+      element: (
+        <MainLayout>
+          <NotFound />
         </MainLayout>
       )
     }
