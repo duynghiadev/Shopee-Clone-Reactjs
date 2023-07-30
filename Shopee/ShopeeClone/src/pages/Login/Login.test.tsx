2748371501114f1f8f7a0fc885ef383edcde9952
duynghiadev <duynghia22302@gmail.com>
2023-07-31 06:52:59 +0700

    238. Test lỗi nhập data không đúng định dạng trên form

--- Shopee/ShopeeClone/src/pages/Login/Login.test.tsx
+++ Shopee/ShopeeClone/src/pages/Login/Login.test.tsx
@@ -1,26 +1,51 @@
-import matchers from '@testing-library/jest-dom/matchers'
-import { screen, waitFor } from '@testing-library/react'
+import { screen, waitFor, fireEvent } from '@testing-library/react'
 import path from 'src/constants/path'
 import { logScreen, renderWithRouter } from 'src/utils/testUtils'
-import { describe, expect, it } from 'vitest'
+import { beforeAll, describe, expect, it } from 'vitest'
+import matchers from '@testing-library/jest-dom/matchers'
 
 expect.extend(matchers)
 
 describe('Login', () => {
-  it('Hiển thị lỗi required khi không nhập gì', async () => {
-    const { user } = renderWithRouter({
+  beforeAll(async () => {
+    renderWithRouter({
       route: path.login
     })
-
     await waitFor(() => {
       expect(screen.queryByPlaceholderText('Email')).toBeInTheDocument()
     })
+  })
 
+  it('Hiển thị lỗi required khi không nhập gì', async () => {
     const submitButton = document.querySelector('form button[type="submit"]') as Element
-    user.click(submitButton)
-    expect(await screen.findByText('Email là bắt buộc')).toBeTruthy()
-    expect(await screen.findByText('Password là bắt buộc')).toBeTruthy()
+    fireEvent.submit(submitButton)
+    await waitFor(async () => {
+      expect(await screen.findByText('Email là bắt buộc')).toBeTruthy()
+      expect(await screen.findByText('Password là bắt buộc')).toBeTruthy()
+    })
+  })
 
+  it('Hiển thị lỗi required khi không nhập gì', async () => {
+    const emailInput = document.querySelector('form input[type="email"]') as HTMLInputElement
+    const passwordInput = document.querySelector('form input[type="password"]') as HTMLInputElement
+    const submitButton = document.querySelector('form button[type="submit"]') as HTMLButtonElement
+
+    fireEvent.change(emailInput, {
+      target: {
+        value: 'test@mail'
+      }
+    })
+
+    fireEvent.change(passwordInput, {
+      target: {
+        value: '123'
+      }
+    })
+
+    fireEvent.submit(submitButton)
+
+    expect(await screen.findByText('Email không đúng định dạng')).toBeTruthy()
+    expect(await screen.findByText('Độ dài từ 6 - 160 ký tự')).toBeTruthy()
     await logScreen()
   })
 })
