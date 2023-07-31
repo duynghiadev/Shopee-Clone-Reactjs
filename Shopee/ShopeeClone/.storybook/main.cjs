c17a58b44c375337732a951eaee9dc012cc2348c
duynghiadev <duynghia22302@gmail.com>
2023-07-31 16:22:44 +0700

    244. Storybook là gì và cài đặt storybook

--- Shopee/ShopeeClone/.storybook/main.cjs
+++ Shopee/ShopeeClone/.storybook/main.cjs
@@ -1,28 +1,11 @@
-const { mergeConfig } = require('vite')
-const path = require('path')
-
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    'storybook-addon-react-router-v6'
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-vite'
   },
   features: {
     storyStoreV7: true
-  },
-  async viteFinal(config) {
-    return mergeConfig(config, {
-      resolve: {
-        alias: {
-          src: path.resolve(__dirname, '../src')
-        }
-      }
-    })
   }
 }
