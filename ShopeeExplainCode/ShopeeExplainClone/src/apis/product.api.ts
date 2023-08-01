7054097fdbb60c980c44381b249b9e44c5044e68
duynghiadev <duynghia22302@gmail.com>
2023-08-02 06:27:33 +0700

    Giải thích file code product.api.ts

--- ShopeeExplainCode/ShopeeExplainClone/src/apis/product.api.ts
+++ ShopeeExplainCode/ShopeeExplainClone/src/apis/product.api.ts
@@ -3,6 +3,7 @@ import { SuccessResponse } from 'src/types/utils.type'
 import http from 'src/utils/http'
 
 const URL = 'products'
+
 const productApi = {
   getProducts(params: ProductListConfig) {
     return http.get<SuccessResponse<ProductList>>(URL, {
