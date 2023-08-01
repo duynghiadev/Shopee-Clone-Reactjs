c2cc85360cda626b502342f016bb7ec1b225bae9
duynghiadev <duynghia22302@gmail.com>
2023-08-01 13:00:21 +0700

    249. Fix Lỗi Input Min Max không bị reset khi nhấn xóa tất cả

--- Shopee/ShopeeClone/src/components/InputNumber/InputNumber.tsx
+++ Shopee/ShopeeClone/src/components/InputNumber/InputNumber.tsx
@@ -13,7 +13,7 @@ const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(function Inpu
     classNameInput = 'p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm',
     classNameError = 'mt-1 text-red-600 min-h-[1.25rem] text-sm',
     onChange,
-    value = '',
+    value,
     ...rest
   },
   ref
@@ -32,7 +32,13 @@ const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(function Inpu
 
   return (
     <div className={className}>
-      <input className={classNameInput} onChange={handleChange} value={value || localValue} {...rest} ref={ref} />
+      <input
+        className={classNameInput}
+        onChange={handleChange}
+        value={value === undefined ? localValue : value}
+        {...rest}
+        ref={ref}
+      />
       <div className={classNameError}>{errorMessage}</div>
     </div>
   )
--- Shopee/ShopeeClone/src/pages/ProductList/components/AsideFilter/AsideFilter.tsx
+++ Shopee/ShopeeClone/src/pages/ProductList/components/AsideFilter/AsideFilter.tsx
@@ -34,7 +34,8 @@ export default function AsideFilter({ queryConfig, categories }: Props) {
     control,
     handleSubmit,
     trigger,
-    formState: { errors }
+    formState: { errors },
+    reset
   } = useForm<FormData>({
     defaultValues: {
       price_min: '',
@@ -55,6 +56,7 @@ export default function AsideFilter({ queryConfig, categories }: Props) {
   })
 
   const handleRemoveAll = () => {
+    reset()
     navigate({
       pathname: path.home,
       search: createSearchParams(omit(queryConfig, ['price_min', 'price_max', 'rating_filter', 'category'])).toString()
