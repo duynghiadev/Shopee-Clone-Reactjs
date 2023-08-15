545527d13a69660d0520ef543a2f81bdf7375dc8
duynghiadev <duynghia22302@gmail.com>
2023-08-15 18:49:44 +0700

    Giải thích file code rules.ts (1)

--- ShopeeExplainCode/ShopeeExplainClone/src/utils/rules.ts
+++ ShopeeExplainCode/ShopeeExplainClone/src/utils/rules.ts
@@ -59,10 +59,15 @@ export const getRules = (getValues?: UseFormGetValues<any>): Rules => ({
 })
 
 function testPriceMinMax(this: yup.TestContext<AnyObject>) {
-  const { price_max, price_min } = this.parent as { price_min: string; price_max: string }
+  const { price_max, price_min } = this.parent as {
+    price_min: string
+    price_max: string
+  }
+
   if (price_min !== '' && price_max !== '') {
     return Number(price_max) >= Number(price_min)
   }
+
   return price_min !== '' || price_max !== ''
 }
 
@@ -82,22 +87,27 @@ export const schema = yup.object({
     .email('Email không đúng định dạng')
     .min(5, 'Độ dài từ 5 - 160 ký tự')
     .max(160, 'Độ dài từ 5 - 160 ký tự'),
+
   password: yup
     .string()
     .required('Password là bắt buộc')
     .min(6, 'Độ dài từ 6 - 160 ký tự')
     .max(160, 'Độ dài từ 6 - 160 ký tự'),
+
   confirm_password: handleConfirmPasswordYup('password'),
+
   price_min: yup.string().test({
     name: 'price-not-allowed',
     message: 'Giá không phù hợp',
     test: testPriceMinMax
   }),
+
   price_max: yup.string().test({
     name: 'price-not-allowed',
     message: 'Giá không phù hợp',
     test: testPriceMinMax
   }),
+
   name: yup.string().trim().required('Tên sản phẩm là bắt buộc')
 })
 
