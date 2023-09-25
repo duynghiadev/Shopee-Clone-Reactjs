eb3bd232fe06e4687a96b830710d1c8c0a48efba
duynghiadev <duynghia22302@gmail.com>
2023-09-26 03:31:06 +0700

    20. Mini CSS extract plugin

--- Again - Reactjs Super/NodeVaNPM/webpack.config.js
+++ Again - Reactjs Super/NodeVaNPM/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "production",
@@ -14,11 +15,12 @@ module.exports = {
     rules: [
       {
         test: /\.s[ac]ss|css$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       title: "Webpack App",
       filename: "index.html",
